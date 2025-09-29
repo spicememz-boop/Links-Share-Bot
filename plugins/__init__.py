2c3a07154205ae1b744c2673b65a4a56fba0942b
ʏᴀᴛᴏ <YatoSama064@gmail.com>
2025-09-29 16:02:08 +0530

    Update __init__.py

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -6,4 +6,4 @@ async def web_server():
     web_app = web.Application(client_max_size=30000000)
     web_app.add_routes(routes)
     return web_app
-# +++ Modified By Yato [telegram username: @i_killed_my_clan & @ProYato] +++ # aNDI BANDI SANDI JISNE BHI CREDIT HATAYA USKI BANDI RAndi 
+# +++ Modified By [telegram username: @Codeflix_Bots
