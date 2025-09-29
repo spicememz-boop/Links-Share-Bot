e1a006fc6a67da108ad16113be50f67d05358b2b
ʏᴀᴛᴏ <YatoSama064@gmail.com>
2025-09-29 16:01:28 +0530

    Update fsub.py

--- plugins/fsub.py
+++ plugins/fsub.py
@@ -1,3 +1,1 @@
-# +++ Modified By Yato [telegram username: @i_killed_my_clan & @ProYato] +++ # aNDI BANDI SANDI JISNE BHI CREDIT HATAYA USKI BANDI RAndi 
 
-# gand marao, public repo me na krha me add force sub 
