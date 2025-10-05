0c8dade9d69d1fae655a84323fc9fb11091c325f
rohit3607 <rohitplayer87089@gmail.com>
2025-10-05 19:20:15 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -7,7 +7,8 @@ from pyrogram import Client, filters
 from pyrogram.enums import ParseMode, ChatMemberStatus, ChatAction
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery, InputMediaPhoto
 from pyrogram.errors import FloodWait, UserNotParticipant
-
+from pyrogram.errors import UserIsBlocked, InputUserDeactivated, FloodWait, PeerIdInvalid, RPCError
+from pyrogram.errors.exceptions.bad_request_400 import UserNotParticipant
 from bot import Bot
 from datetime import datetime, timedelta
 from config import *
